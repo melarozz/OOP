27710d968b3926a773f3ee94c645007755ee4665
melarozz <v.yakovleva2@g.nsu.ru>
2023-09-18 15:12:39 +0700

    added tests

--- Task_1_1_1/src/test/java/yakovleva/Heapsorttest.java
+++ Task_1_1_1/src/test/java/yakovleva/Heapsorttest.java
@@ -1,6 +1,9 @@
 package yakovleva;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class HeapSortTest {
@@ -36,5 +39,41 @@ class HeapSortTest {
         Heapsort.sift(arr, arr.length, 0);
         assertArrayEquals(arr, expected);
     }
+
+    @Test
+    void checkLargeInput() {
+        int[] arr = new int[1000000];
+        int[] expected = new int[1000000];
+        Arrays.fill(arr,0);
+        Arrays.fill(expected,0);
+        int[] result = Heapsort.heapsort(arr);
+        assertArrayEquals(result, expected);
+    }
+
+    @Test
+    void checkSingleElement() {
+        int[] arr = { 5 };
+        int[] expected = { 5 };
+        int[] result = Heapsort.heapsort(arr);
+        assertArrayEquals(result, expected);
+    }
+
+    @Test
+    void checkNegativeNumbers() {
+        int[] arr = { -5, -2, -8, -1, -3 };
+        int[] expected = { -8, -5, -3, -2, -1 };
+        int[] result = Heapsort.heapsort(arr);
+        assertArrayEquals(result, expected);
+    }
+
+    @Test
+    void checkSortedArray() {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] expected = { 1, 2, 3, 4, 5 };
+        int[] result = Heapsort.heapsort(arr);
+        assertArrayEquals(result, expected);
+    }
+
+
 
 }
