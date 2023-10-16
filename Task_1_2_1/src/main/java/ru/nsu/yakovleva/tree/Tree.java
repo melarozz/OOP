bb00ce6b874c1f46d45dc7bcd3967ba159ccbcea
melarozz <v.yakovleva2@g.nsu.ru>
2023-10-16 15:26:51 +0700

    task 2 1

--- Task_1_2_1/src/main/java/ru/nsu/yakovleva/tree/Tree.java
+++ Task_1_2_1/src/main/java/ru/nsu/yakovleva/tree/Tree.java
@@ -1,7 +1,7 @@
 package ru.nsu.yakovleva.tree;
 
-import java.util.ConcurrentModificationException;
 import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
