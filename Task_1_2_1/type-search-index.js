b8e6e6fc416b67d52f675711f48cd63ac8225957
melarozz <melarozz@users.noreply.github.com>
2023-10-16 08:19:02 +0000

    Deploying to gh-pages from @ melarozz/OOP@ccac2173975f9075917949606b48a66b36cb5c1b 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.yakovleva.tree","l":"Tree"},{"p":"ru.nsu.yakovleva.tree","l":"TreeIterator"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.yakovleva.tree","l":"Tree"}]
