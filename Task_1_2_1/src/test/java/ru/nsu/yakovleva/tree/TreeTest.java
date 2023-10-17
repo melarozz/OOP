11f20396eecf0bff491ddb3f41eae597153d452f
melarozz <v.yakovleva2@g.nsu.ru>
2023-10-17 09:50:17 +0700

    checkstyle

--- Task_1_2_1/src/test/java/ru/nsu/yakovleva/tree/TreeTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/yakovleva/tree/TreeTest.java
@@ -1,8 +1,8 @@
 package ru.nsu.yakovleva.tree;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
