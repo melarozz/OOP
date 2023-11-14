01c3d80a66357bd28ff55b53c6eb98ce9be2bb84
melarozz <melarozz@users.noreply.github.com>
2023-11-14 01:54:27 +0000

    Deploying to gh-pages from @ melarozz/OOP@8caaca3759330051d59fa2509ab9ccdb1e2966cc 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.yakovleva.string","c":"SubstringFinder","l":"boyerMooreSearch(String, String)","url":"boyerMooreSearch(java.lang.String,java.lang.String)"},{"p":"ru.nsu.yakovleva.string","c":"SubstringFinder","l":"SubstringFinder()","url":"%3Cinit%3E()"}]
