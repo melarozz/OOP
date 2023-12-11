cbd519f28327f28f3d5771c3d184928feac22ed6
melarozz <melarozz18@gmail.com>
2023-12-11 22:12:55 +0700

    pattern matching and tests

--- Task_1_5_1/src/main/java/ru/nsu/yakovleva/calculator/Main.java
+++ Task_1_5_1/src/main/java/ru/nsu/yakovleva/calculator/Main.java
@@ -3,94 +3,95 @@ package ru.nsu.yakovleva.calculator;
 import java.lang.Math;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 /**
  * A simple calculator that evaluates expressions in prefix form.
  */
 public class Main {
 
-    /**
-     * Reads an expression in prefix form from the user and calculates the result.
-     *
-     * @param args Command-line arguments (not used in this application)
-     */
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the expression in prefix form:");
-        String input = scanner.nextLine();
-        scanner.close();
+    enum Flag {
+        NORMAL, DEGREES
+    }
 
-        double result = calculate(input);
-        System.out.println("Output: " + result);
+    private static Flag flag = Flag.NORMAL;
+
+    /**
+     * Sets the flag for the calculator.
+     *
+     * @param newFlag The new flag value (-n or -d)
+     */
+    private static void setFlag(String newFlag) {
+        if (newFlag.equals("-n")) {
+            flag = Flag.NORMAL;
+        } else if (newFlag.equals("-d")) {
+            flag = Flag.DEGREES;
+        } else {
+            throw new IllegalArgumentException("Invalid flag");
+        }
+    }
+
+    /**
+     * Extracts the flag from the expression and sets it accordingly.
+     *
+     * @param expression The expression in prefix form to be evaluated, including the flag
+     * @return The expression without the flag
+     */
+    private static String extractFlag(String expression) {
+        if (expression.startsWith("-n ")) {
+            setFlag("-n");
+            return expression.substring(3); // Remove the flag (-n)
+        } else if (expression.startsWith("-d ")) {
+            setFlag("-d");
+            return expression.substring(3); // Remove the flag (-d)
+        } else {
+            throw new IllegalArgumentException("Flag not provided or invalid");
+        }
     }
 
     /**
      * Evaluates a prefix expression and returns the result.
      *
-     * @param expression The expression in prefix form to be evaluated
+     * @param expressionWithFlag The expression in prefix form with the flag to be evaluated
      * @return The result of the expression evaluation
      * @throws IllegalArgumentException If the expression is invalid
      * @throws ArithmeticException      If there is a division by zero
      */
-    public static double calculate(String expression) {
+    public static double calculate(String expressionWithFlag) {
+        String expression = extractFlag(expressionWithFlag);
         String[] tokens = expression.split("\\s+");
         Deque<Double> stack = new LinkedList<>();
 
         for (int i = tokens.length - 1; i >= 0; i--) {
             String token = tokens[i];
-            if (isNumeric(token)) {
-                stack.push(Double.parseDouble(token));
-            } else {
-                if (token.equals("+")) {
-                    // Addition
-                    double operand1 = stack.pop();
-                    double operand2 = stack.pop();
-                    stack.push(operand1 + operand2);
-                } else if (token.equals("-")) {
-                    // Subtraction
-                    double operand1 = stack.pop();
-                    double operand2 = stack.pop();
-                    stack.push(operand1 - operand2);
-                } else if (token.equals("*")) {
-                    // Multiplication
-                    double operand1 = stack.pop();
-                    double operand2 = stack.pop();
-                    stack.push(operand1 * operand2);
-                } else if (token.equals("/")) {
-                    // Division
-                    double operand1 = stack.pop();
-                    double operand2 = stack.pop();
-                    if (operand2 == 0) {
-                        throw new ArithmeticException("Division by zero");
+            switch (token) {
+                case "+", "-", "*", "/", "sin", "cos", "log" -> {
+                    if (flag == Flag.DEGREES && (token.equals("log"))) {
+                        throw new IllegalArgumentException("Logarithm function in degrees not supported");
                     }
-                    stack.push(operand1 / operand2);
-                } else if (token.equals("sin")) {
-                    // Sin function
-                    double operand = stack.pop();
-                    stack.push(Math.sin(operand));
-                } else if (token.equals("cos")) {
-                    // Cos function
-                    double operand = stack.pop();
-                    stack.push(Math.cos(operand));
-                } else if (token.equals("sind")) {
-                    // Sin function
-                    double operand = stack.pop();
-                    stack.push(Math.sin(Math.toRadians(operand)));
-                } else if (token.equals("cosd")) {
-                    // Cos function
-                    double operand = stack.pop();
-                    stack.push(Math.cos(Math.toRadians(operand)));
-                } else if (token.equals("log")) {
-                    // Logarithm function
-                    double base = stack.pop();
-                    double number = stack.pop();
-                    if (number <= 0 || base <= 0 || base == 1 ) {
-                        throw new ArithmeticException("Invalid arguments");
+                    double operand1, operand2;
+                    switch (token) {
+                        case "+", "-", "*", "/" -> {
+                            operand1 = stack.pop();
+                            operand2 = stack.pop();
+                            stack.push(performOperation(token, operand1, operand2));
+                        }
+                        case "sin", "cos" -> {
+                            operand1 = stack.pop();
+                            stack.push(performTrigonometricFunction(token, operand1));
+                        }
+                        case "log" -> {
+                            double base = stack.pop();
+                            double number = stack.pop();
+                            stack.push(performLogarithmFunction(base, number));
+                        }
                     }
-                    stack.push(Math.log(number) / Math.log(base));
-                } else {
-                    throw new IllegalArgumentException("Invalid expression");
+                }
+                default -> {
+                    if (isNumeric(token)) {
+                        stack.push(Double.parseDouble(token));
+                    } else {
+                        throw new IllegalArgumentException("Invalid expression");
+                    }
                 }
             }
         }
@@ -102,6 +103,71 @@ public class Main {
         return stack.pop();
     }
 
+    /**
+     * Performs arithmetic operations based on the operator.
+     *
+     * @param operator The operator (+, -, *, /)
+     * @param operand1 The first operand
+     * @param operand2 The second operand
+     * @return The result of the arithmetic operation
+     * @throws IllegalArgumentException If the operator is invalid
+     * @throws ArithmeticException      If there is a division by zero
+     */
+    private static double performOperation(String operator, double operand1, double operand2) {
+        switch (operator) {
+            case "+" -> {
+                return operand1 + operand2;
+            }
+            case "-" -> {
+                return operand1 - operand2;
+            }
+            case "*" -> {
+                return operand1 * operand2;
+            }
+            case "/" -> {
+                if (operand2 == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                return operand1 / operand2;
+            }
+            default -> throw new IllegalArgumentException("Invalid operator");
+        }
+    }
+
+    /**
+     * Performs trigonometric functions based on the specified function.
+     *
+     * @param function The trigonometric function (sin, cos, sind, cosd)
+     * @param operand  The operand for the trigonometric function
+     * @return The result of the trigonometric function
+     * @throws IllegalArgumentException If the function is invalid
+     */
+    private static double performTrigonometricFunction(String function, double operand) {
+        switch (function) {
+            case "sin" -> {
+                return flag == Flag.DEGREES ? Math.sin(Math.toRadians(operand)) : Math.sin(operand);
+            }
+            case "cos" -> {
+                return flag == Flag.DEGREES ? Math.cos(Math.toRadians(operand)) : Math.cos(operand);
+            }
+            default -> throw new IllegalArgumentException("Invalid function");
+        }
+    }
+
+    /**
+     * Performs logarithmic functions based on the specified base and number.
+     *
+     * @param base   The base of the logarithm
+     * @param number The number for the logarithm
+     * @return The result of the logarithmic function
+     * @throws IllegalArgumentException If the arguments are invalid
+     */
+    private static double performLogarithmFunction(double base, double number) {
+        if (number <= 0 || base <= 0 || base == 1) {
+            throw new ArithmeticException("Invalid arguments");
+        }
+        return Math.log(number) / Math.log(base);
+    }
 
     /**
      * Checks if a given string is numeric.
--- Task_1_5_1/src/test/java/ru/nsu/yakovleva/calculator/CalcTest.java
+++ Task_1_5_1/src/test/java/ru/nsu/yakovleva/calculator/CalcTest.java
@@ -3,9 +3,7 @@ package ru.nsu.yakovleva.calculator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
-
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,144 +13,174 @@ public class CalcTest {
 
     @Test
     void testBasicExpression() {
-        String expression = "sin + - 1 2 1";
+        String expression = "-n sin + - 1 2 1";
+        double result = Main.calculate(expression);
+        assertEquals(0, result, 0.0001);
+    }
+
+    @Test
+    void testBasicExpressionDegrees() {
+        String expression = "-d sin + -10 10";
         double result = Main.calculate(expression);
         assertEquals(0, result, 0.0001);
     }
 
     @Test
     void testLog() {
-        String expression = "log 2 8";
+        String expression = "-n log 2 8";
         double result = Main.calculate(expression);
         assertEquals(3, result, 0.0001);
     }
 
     @Test
     void testAllOperations() {
-        String expression = "+ * sin - cos 0 1 / cos 1 4 log 2 8";
+        String expression = "-n + * sin - cos 0 1 / cos 1 4 log 2 8";
         double result = Main.calculate(expression);
         assertEquals(3, result, 0.0001);
     }
 
     @Test
     void testExpression() {
-        String expression = "/ * + - + log 2 8 1 6 1 9 3";
+        String expression = "-n / * + - + log 2 8 1 6 1 9 3";
         double result = Main.calculate(expression);
         assertEquals(-3, result, 0.0001);
     }
 
     @Test
     void testAddition() {
-        String expression = "+ 5 3";
+        String expression = "-n + 5 3";
         double result = Main.calculate(expression);
         assertEquals(8, result, 0.0001);
     }
 
     @Test
     void testSubtraction() {
-        String expression = "- 5 3";
+        String expression = "-n - 5 3";
         double result = Main.calculate(expression);
         assertEquals(2, result, 0.0001);
     }
 
     @Test
     void testMultiplication() {
-        String expression = "* 5 3";
+        String expression = "-n * 5 3";
         double result = Main.calculate(expression);
         assertEquals(15, result, 0.0001);
     }
 
     @Test
     void testDivision() {
-        String expression = "/ 10 2";
+        String expression = "-n / 10 2";
         double result = Main.calculate(expression);
         assertEquals(5, result, 0.0001);
     }
 
     @Test
     void testSineFunction() {
-        String expression = "sin 0";
+        String expression = "-n sin 0";
         double result = Main.calculate(expression);
         assertEquals(0, result, 0.0001);
     }
 
     @Test
     void testSineDegrees0Function() {
-        String expression = "sind 0";
+        String expression = "-d sin 0";
         double result = Main.calculate(expression);
         assertEquals(0, result, 0.0001);
     }
 
     @Test
     void testSineDegrees90Function() {
-        String expression = "sind 90";
+        String expression = "-d sin 90";
         double result = Main.calculate(expression);
         assertEquals(1, result, 0.0001);
     }
 
     @Test
     void testSineDegrees30Function() {
-        String expression = "sind 30";
+        String expression = "-d sin 30";
         double result = Main.calculate(expression);
         assertEquals(0.5, result, 0.0001);
     }
 
     @Test
     void testCosineFunction() {
-        String expression = "cos 0";
+        String expression = "-n cos 0";
         double result = Main.calculate(expression);
         assertEquals(1, result, 0.0001);
     }
 
     @Test
     void testCosineDegrees0Function() {
-        String expression = "cosd 0";
+        String expression = "-d cos 0";
         double result = Main.calculate(expression);
         assertEquals(1, result, 0.0001);
     }
 
     @Test
     void testCosineDegrees90Function() {
-        String expression = "cosd 90";
+        String expression = "-d cos 90";
         double result = Main.calculate(expression);
         assertEquals(0, result, 0.0001);
     }
 
     @Test
     void testCosineDegrees60Function() {
-        String expression = "cosd 60";
+        String expression = "-d cos 60";
         double result = Main.calculate(expression);
         assertEquals(0.5, result, 0.0001);
     }
 
     @Test
     void testDivisionByZero() {
-        String expression = "/ 5 0";
+        String expression = "-n / 5 0";
         assertThrows(ArithmeticException.class, () -> Main.calculate(expression));
     }
 
     @Test
     void testEmptyStackException() {
-        String expression = "log 10"; // Log requires two operands
+        String expression = "-n log 10"; // Log requires two operands
         assertThrows(NoSuchElementException.class, () -> Main.calculate(expression));
     }
 
     @Test
     void testLogInvalidBaseException() {
-        String expression = "log -2 10";
+        String expression = "-n log -2 10";
         assertThrows(ArithmeticException.class, () -> Main.calculate(expression));
     }
 
     @Test
     void testLogInvalidBase2Exception() {
-        String expression = "log 1 10";
+        String expression = "-n log 1 10";
         assertThrows(ArithmeticException.class, () -> Main.calculate(expression));
     }
 
     @Test
     void testLogInvalidNumberException() {
-        String expression = "log 2 -10";
+        String expression = "-n log 2 -10";
         assertThrows(ArithmeticException.class, () -> Main.calculate(expression));
     }
 
+    @Test
+    void testLogDegreesException() {
+        String expression = "-d log 2 8";
+        assertThrows(IllegalArgumentException.class, () -> Main.calculate(expression));
+    }
+
+    @Test
+    void testLogInvalidFlagException() {
+        String expression = "-t log 2 -10";
+        assertThrows(IllegalArgumentException.class, () -> Main.calculate(expression));
+    }
+
+    @Test
+    void testNoFlagException() {
+        String expression = "log 2 -10";
+        assertThrows(IllegalArgumentException.class, () -> Main.calculate(expression));
+    }
+
+    @Test
+    void testLettersException() {
+        String expression = "+ a b";
+        assertThrows(IllegalArgumentException.class, () -> Main.calculate(expression));
+    }
 }
