4f8371ab7e8eaab22ab783746c160d32cf4cfe86
melarozz <melarozz18@gmail.com>
2023-12-17 18:20:53 +0700

    tests with mockito

--- Task_1_5_2/src/test/java/ru/nsu/yakovleva/NotebookAppTest.java
+++ Task_1_5_2/src/test/java/ru/nsu/yakovleva/NotebookAppTest.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * Test class.
@@ -73,6 +74,63 @@ class NotebookAppTest {
 
         assertDoesNotThrow(() -> notebookApp.show("2023.01.01 00:00",
                 "2024.01.01 00:00", new String[]{"keyword"}));
+    }
+
+    @Test
+    void helpCommandDisplaysHelpInformation() {
+        String[] args = {"-help"};
+        assertDoesNotThrow(() -> notebookApp.run(args));
+    }
+
+    @Test
+    void performMultipleOperations() throws IOException {
+        when(writer.getFileName()).thenReturn("testFile.json");
+        doNothing().when(writer).open();
+        doNothing().when(writer).close();
+        when(writer.read()).thenReturn(new Note[]{});
+
+        String[] args = {"-add", "Title1", "Content1", "-remove", "Title1", "-show"};
+        assertDoesNotThrow(() -> notebookApp.run(args));
+    }
+
+    @Test
+    void showAllNotes() throws IOException {
+        when(writer.getFileName()).thenReturn("testFile.json");
+        doNothing().when(writer).open();
+        doNothing().when(writer).close();
+        when(writer.read()).thenReturn(new Note[]{
+                new Note("Title 1", "Content 1"),
+                new Note("Title 2", "Content 2")
+        });
+
+        assertDoesNotThrow(() -> notebookApp.show());
+    }
+
+    @Test
+    void showNotesByDateRange() throws IOException {
+        when(writer.getFileName()).thenReturn("testFile.json");
+        doNothing().when(writer).open();
+        doNothing().when(writer).close();
+        when(writer.read()).thenReturn(new Note[]{
+                new Note("Title 1", "Content 1"),
+                new Note("Title 2", "Content 2")
+        });
+
+        assertDoesNotThrow(() -> notebookApp.show("2023.01.01 00:00", "2024.01.01 00:00"));
+    }
+
+    @Test
+    void showNotesByDateAndKeywords() throws IOException {
+        when(writer.getFileName()).thenReturn("testFile.json");
+        doNothing().when(writer).open();
+        doNothing().when(writer).close();
+        when(writer.read()).thenReturn(new Note[]{
+                new Note("Title 1", "Content with keyword"),
+                new Note("Title 2", "Content without keyword")
+        });
+
+        assertDoesNotThrow(() -> notebookApp.show("2023.01.01 00:00",
+                "2024.01.01 00:00", new String[]{"Title"}));
     }
 
 }
