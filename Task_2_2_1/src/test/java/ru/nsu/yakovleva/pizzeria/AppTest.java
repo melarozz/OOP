207954ef070dac2459671a04e26052e130fa196d
melarozz <melarozz18@gmail.com>
2024-03-15 02:27:32 +0700

    tests

---
+++ Task_2_2_1/src/test/java/ru/nsu/yakovleva/pizzeria/AppTest.java
@@ -0,0 +1,33 @@
+package ru.nsu.yakovleva.pizzeria;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void testAppExecuteClass() {
+        ApplicationExecution appExecute = new ApplicationExecution();
+        assertNotNull(appExecute);
+    }
+
+
+}
